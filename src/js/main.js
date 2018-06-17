22f0763681bf2bef662867cd5939c353e0d5387d
DanilDerevyanchenko <34552910+DanilDerevyanchenko@users.noreply.github.com>
2018-06-17 22:55:52 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,144 @@
 $(document).ready(function(){
-  $('.about__slider').slick({
+  $('.main-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          arrows: false,
+          dots: true 
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          dots: true
+        }
+      },
+       {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+    $('.featured-slider').slick({
     dots: false,
     arrows: true,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    draggable: false,
+    appendArrows: ".featured__arrow",
+    prevArrow: '<button class="featured__arrow-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="featured__arrow-next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          draggable: false,
+          autoplay: false,
+          arrows: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          dots: false
+        }
+      },
+       {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          draggable: false,
+          arrows: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+    $('.new-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    appendArrows: ".new__arrow",
+    prevArrow: '<button class="new__arrow-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="new__arrow-next"><i class="fas fa-angle-right"></i></button>',
     responsive: [
       {
         breakpoint: 1600,
@@ -22,7 +156,7 @@ $(document).ready(function(){
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          infinite: false,
+          infinite: true,
           autoplay: false,
           dots: false
         }
@@ -61,12 +195,16 @@ $(document).ready(function(){
     ]
   });
 
-    $('.reviews__slider').slick({
-    dots: true,
-    arrows: false,
+   $('.recently-slider').slick({
+    dots: false,
+    arrows: true,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    draggable: false,
+    appendArrows: ".recently__arrow",
+    prevArrow: '<button class="recently__arrow-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="recently__arrow-next"><i class="fas fa-angle-right"></i></button>',
     responsive: [
       {
         breakpoint: 1600,
@@ -75,8 +213,8 @@ $(document).ready(function(){
           slidesToScroll: 1,
           infinite: true,
           autoplay: false,
-          arrows: false,
-          dots: true
+          arrows: true,
+          dots: false
         }
       },
       {
@@ -84,9 +222,140 @@ $(document).ready(function(){
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          infinite: false,
+          infinite: true,
           autoplay: false,
-          dots: true
+          dots: false
+        }
+      },
+       {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true,
+          autoplay: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  $('.bslider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    prevArrow: '<button class="bslider__arrow-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="bslider__arrow-next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          arrows: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          dots: false
+        }
+      },
+       {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+          autoplay: false,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+    $('.footer__slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    appendArrows: ".footer__arrow",
+    prevArrow: '<button class="footer__arrow-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button class="footer__arrow-next"><i class="fas fa-angle-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          arrows: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          autoplay: false,
+          dots: false
         }
       },
        {
@@ -126,8 +395,8 @@ $(document).ready(function(){
  });
 
 
-$(function() {
-  $(".featured__block").click(function() {
-    $(".featured__block--submenu").slideToggle(500);
-  });
-});
+// $(function() {
+//   $(".featured__block").click(function() {
+//     $(".featured__block--submenu").slideToggle(500);
+//   });
+// });
