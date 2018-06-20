143b0ecaaff430d90d8d2659f2a3cda8bdcfd230
DanilDerevyanchenko <34552910+DanilDerevyanchenko@users.noreply.github.com>
2018-06-21 02:37:57 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -394,6 +394,8 @@ $(document).ready(function(){
 
  });
 
+
+
  $(document).ready(function() {
    var link        = $('.menu__toggle');
    var link_active = $('.menu__toggle-active');
@@ -407,13 +409,43 @@ $(document).ready(function(){
        link.toggleClass('menu__toggle-active');
        menu.toggleClass('account__menu_active');
     });
+
+    $(".main__menu--button").on("click", function() {
+      $(".main__list").slideToggle(500);
+      $(".main__menu--button i").toggleClass("arrow-top");
+    });
  });
 
  $(document).ready(function() {
   $(".main__item--active").click(function() {
     $(".main__filter").slideToggle(500);
   });
+
+  $(".main__item").on("click", function(e) {
+    e.preventDefault();
+    $(".main__item").removeClass("active__link");
+    $(this).addClass("active__link");
+  });
+  $("body").on("click", function() {
+    $(".main__item").removeClass("active__link");
+  });
+
+
+  $(".account__language").click(function() {
+     $(".account__language--arrow").toggleClass(".account__language--active");
+    $(".account__language--submenu").slideToggle(500);
+  });
+
+  $(".account__money").click(function() {
+    $(".account__money--submenu").slideToggle(500);
+    $(".account__money--arrow").toggleClass(".account__money--active");
+  });
  });
+
+ // $(function (){
+ //  var mobHeader = $('<div class="main__menu--active"><ul class="main__list"><li class="main__item main__item--active"><a class="main__link main__link--active"><img src="img/main-list.svg" alt="">all categories</a></li><li class="main__item"><a href="#" class="main__link">Brands</a></li><li class="main__item"><a href="#" class="main__link">Deals of the Day</a></li><li class="main__item"><a href="#" class="main__link">News</a></li><li class="main__item"><a href="#" class="main__link">Sale</a></li></ul></div>');
+ //  $("account__list").before(mobHeader);
+ // });
 
 
 // $(function() {
